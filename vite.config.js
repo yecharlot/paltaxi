2862dd64c570c32a7aa7d28dba24ff1e4c14ce4a
yecharlot <152829220+yecharlot@users.noreply.github.com>
2025-08-17 03:53:07 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,7 @@ export default defineConfig({
       ],server: {
     host: '0.0.0.0',
     port: 5173,
-         allowedHosts: [
-      'paltaxi.onrender.com', // Permite tu dominio de Render
-    ],
+         allowedHosts: 'all'
   }
     },
   },
