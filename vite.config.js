8a5bce0ea1a515838b8fd43d78c02649d2323e6b
yecharlot <152829220+yecharlot@users.noreply.github.com>
2025-08-17 00:54:26 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss'; // 👈 Importa Tailwind
+import autoprefixer from 'autoprefixer'; // 👈 Importa Autoprefixer
 
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(), // 👈 Usa Tailwind como plugin
+        autoprefixer(), // 👈 Usa Autoprefixer
+      ],
+    },
+  },
+});
